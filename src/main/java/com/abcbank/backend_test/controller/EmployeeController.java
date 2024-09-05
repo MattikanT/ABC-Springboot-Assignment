8c075108d414286391741c9abe0b53a2bf2c4e58
Mattikan Tongsai <mattikantongsai@gmail.com>
2024-09-05 23:52:52 +0300

    move logic to service

--- src/main/java/com/abcbank/backend_test/controller/EmployeeController.java
+++ src/main/java/com/abcbank/backend_test/controller/EmployeeController.java
@@ -3,15 +3,8 @@ package com.abcbank.backend_test.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abcbank.backend_test.model.Employee;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import com.abcbank.backend_test.service.EmployeeService;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,47 +17,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class EmployeeController {
 
-    // Path to the JSON file where employee data is stored
-    private static final String JSON_FILE_PATH = "employees.json";
-
-    // A list to store employees in memory during runtime
-    private List<Employee> employees = new ArrayList<>();
-
-    public EmployeeController() {
-        // When the application starts, load employees from the JSON file
-        this.employees = readEmployeesFromFile();
-        // Sample employee data
-        // this.employees = new ArrayList<>();
-        // employees.add(new Employee(1, "Alice", "Ahmed", "2000-01-01", "7000", "2023-05-18", "IT"));
-        // employees.add(new Employee(2, "Mail", "Tongsai", "2000-01-01", "5000", "2023-05-18", "IT"));
-        // employees.add(new Employee(3, "Eslam", "Allam", "2000-01-01", "1000", "2023-05-18", "IT"));
-    }
-
+    private EmployeeService employeeService = new EmployeeService();
     
     // POST request to insert a new employee
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
    
-        int newId = employees.stream()
-                .mapToInt(Employee::getId) 
-                .max() 
-                .orElse(0) + 1; 
 
-        employee.setId(newId);
-        employees.add(employee);
-        writeEmployeesToFile(employees);
-        
-        return employee;
+        return employeeService.createEmployee(employee);
     }
     
+
 
     // Get request to get employee by ID
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable Integer id) {
-        return employees.stream()
-                .filter(employee -> employee.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        return employeeService.getEmployeeById(id);
     }
 
 
@@ -76,41 +44,8 @@ public class EmployeeController {
             @RequestParam(required = false) String fromSalary,
             @RequestParam(required = false) String toSalary) {
 
-        return employees.stream()
-                .filter(employee -> (name == null || employee.getFirstName().toLowerCase().contains(name.toLowerCase()) 
-                                    || employee.getLastName().toLowerCase().contains(name.toLowerCase())))// Filter by name
-                .filter(employee -> (fromSalary == null
-                        || Double.parseDouble(employee.getSalary()) >= Double.parseDouble(fromSalary))) // Filter by
-                                                                                                        // fromSalary
-                .filter(employee -> (toSalary == null
-                        || Double.parseDouble(employee.getSalary()) <= Double.parseDouble(toSalary))) // Filter by
-                                                                                                      // toSalary
-                .collect(Collectors.toList()); // Collect filtered list
+      return employeeService.getEmployeesByFilter(name, fromSalary, toSalary);
 
     }
-
-
-
-
-        // Helper methods to read/write JSON
-    private void writeEmployeesToFile(List<Employee> users) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writeValue(new File(JSON_FILE_PATH), users);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<Employee> readEmployeesFromFile() {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(new File(JSON_FILE_PATH),
-                    new TypeReference<List<Employee>>() {});
-        } catch (IOException e) {
-            return new ArrayList<>();
-        }
-    }
-    
 
 }
--- src/main/java/com/abcbank/backend_test/service/EmployeeService.java
+++ src/main/java/com/abcbank/backend_test/service/EmployeeService.java
@@ -1,5 +1,84 @@
 package com.abcbank.backend_test.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.abcbank.backend_test.model.Employee;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class EmployeeService {
     
+    private static final String JSON_FILE_PATH = "employees.json";
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeService() {
+        this.employees = readEmployeesFromFile();
+  
+    }
+
+    private void writeEmployeesToFile(List<Employee> users) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File(JSON_FILE_PATH), users);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Employee> readEmployeesFromFile() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new File(JSON_FILE_PATH),
+                    new TypeReference<List<Employee>>() {});
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+    
+
+    // Insert a new employee
+    public Employee createEmployee( Employee employee){
+        int newId = employees.stream()
+                .mapToInt(Employee::getId)
+                .max()
+                .orElse(0) + 1;
+
+        employee.setId(newId);
+        employees.add(employee);
+        writeEmployeesToFile(employees);
+
+        return employee;
+
+    }
+
+    // Get employee by ID
+    public Employee getEmployeeById(Integer id){
+        return employees.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    // Filter employees by name, fromSalary, and toSalary
+    public List<Employee> getEmployeesByFilter( String name, String fromSalary, String toSalary ){
+
+              return employees.stream()
+                .filter(employee -> (name == null || employee.getFirstName().toLowerCase().contains(name.toLowerCase()) 
+                                    || employee.getLastName().toLowerCase().contains(name.toLowerCase())))// Filter by name
+                .filter(employee -> (fromSalary == null
+                        || Double.parseDouble(employee.getSalary()) >= Double.parseDouble(fromSalary))) // Filter by
+                                                                                                        // fromSalary
+                .filter(employee -> (toSalary == null
+                        || Double.parseDouble(employee.getSalary()) <= Double.parseDouble(toSalary))) // Filter by
+                                                                                                      // toSalary
+                .collect(Collectors.toList()); // Collect filtered list
+
+        }
+
 }
